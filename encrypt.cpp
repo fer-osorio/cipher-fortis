830d81895083ad977cdec0419b95a9ae4dae2af7
Fernando Osorio <alexis.fernando.osorio.sarabio@gmail.com>
2023-10-31 00:08:49 -0600

    Added a function to generate keys. It has to be stated explicitly that this function does not generate secure cryptographic keys.

--- encrypt.cpp
+++ encrypt.cpp
@@ -1,29 +1,33 @@
 #include<iostream>
+#include<random>
 #include"Source/Bitmap.hpp"
 
+char key256[] = {(char)0x60, (char)0x3D, (char)0xEB, (char)0x10,
+                 (char)0x15, (char)0xCA, (char)0x71, (char)0xBE,
+                 (char)0x2B, (char)0x73, (char)0xAE, (char)0xF0,
+                 (char)0x85, (char)0x7D, (char)0x77, (char)0x81,
+                 (char)0x1F, (char)0x35, (char)0x2C, (char)0x07,
+                 (char)0x3B, (char)0x61, (char)0x08, (char)0xD7,
+                 (char)0x2D, (char)0x98, (char)0x10, (char)0xA3,
+                 (char)0x09, (char)0x14, (char)0xDF, (char)0xF4};
+
+char key192[] = {(char)0x8E, (char)0x73, (char)0xB0, (char)0xF7,
+                 (char)0xDA, (char)0x0E, (char)0x64, (char)0x52,
+                 (char)0xC8, (char)0x10, (char)0xF3, (char)0x2B,
+                 (char)0x80, (char)0x90, (char)0x79, (char)0xE5,
+                 (char)0x62, (char)0xF8, (char)0xEA, (char)0xD2,
+                 (char)0x52, (char)0x2C, (char)0x6B, (char)0x7B};
+
+char key128[]= {char(0x2B), char(0x7E), char(0x15), char(0x16),
+                char(0x28), char(0xAE), char(0xD2), char(0xA6),
+                char(0xAB), char(0xF7), char(0x15), char(0x88),
+                char(0x09), char(0xCF), char(0x4F), char(0x3C)};
+
+char* set_key(AESkey::Length len); // Not proved to be secure.
+
 int main(int argc, char *argv[]) {
-    char key256[] = {(char)0x60, (char)0x3D, (char)0xEB, (char)0x10,
-                     (char)0x15, (char)0xCA, (char)0x71, (char)0xBE,
-                     (char)0x2B, (char)0x73, (char)0xAE, (char)0xF0,
-                     (char)0x85, (char)0x7D, (char)0x77, (char)0x81,
-                     (char)0x1F, (char)0x35, (char)0x2C, (char)0x07,
-                     (char)0x3B, (char)0x61, (char)0x08, (char)0xD7,
-                     (char)0x2D, (char)0x98, (char)0x10, (char)0xA3,
-                     (char)0x09, (char)0x14, (char)0xDF, (char)0xF4};
-
-    char key192[] = {(char)0x8E, (char)0x73, (char)0xB0, (char)0xF7,
-                     (char)0xDA, (char)0x0E, (char)0x64, (char)0x52,
-                     (char)0xC8, (char)0x10, (char)0xF3, (char)0x2B,
-                     (char)0x80, (char)0x90, (char)0x79, (char)0xE5,
-                     (char)0x62, (char)0xF8, (char)0xEA, (char)0xD2,
-                     (char)0x52, (char)0x2C, (char)0x6B, (char)0x7B};
-
-    char key128[]= {char(0x2B), char(0x7E), char(0x15), char(0x16),
-                    char(0x28), char(0xAE), char(0xD2), char(0xA6),
-                    char(0xAB), char(0xF7), char(0x15), char(0x88),
-                    char(0x09), char(0xCF), char(0x4F), char(0x3C)};
     char IV[16];
-    if(argc > 1) {
+    if(argc > 1) { // Encrypting Bitmap image.
         Bitmap img(argv[1]);
         AES e(key256, AESkey::_256);
         encryptCBC(img, e, IV);
@@ -36,6 +40,7 @@ int main(int argc, char *argv[]) {
 
     char input[1025]; // Maximum size 1024 characters without EOF.
     char copy[1025];
+    char* key = set_key(AESkey::_256);
     unsigned size = 0, i;
     std::cout << "\nWrite the string you want to encrypt. To process the "
                  "string sent the value 'EOF', which you can do by:\n\n"
@@ -49,7 +54,7 @@ int main(int argc, char *argv[]) {
         }
     }
     input[--size] = 0; // End of string.
-    AES e(key256, AESkey::_256);
+    AES e(key, AESkey::_256);
     // -Setting up a test for the decryption algorithm
     for(i = 0; i < size; i++) {copy[i] = input[i];} copy[size] = 0;
     bool decryptionSuccesfull = true;
@@ -61,7 +66,8 @@ int main(int argc, char *argv[]) {
     e.decryptCBC(input, size, IV);
     std::cout << "\nDecryption::\n" << input << '\n';
 
-    e = AES(key192, AESkey::_192);
+    key = set_key(AESkey::_192);
+    e = AES(key, AESkey::_192);
 
     e.encryptCBC(input, size, IV);
     std::cout << "\nEncryption with a key of 192 bits:\n" << input << '\n';
@@ -70,7 +76,8 @@ int main(int argc, char *argv[]) {
     e.decryptCBC(input, size, IV);
     std::cout << "\nDecryption::\n" << input << '\n';
 
-    e = AES(key128, AESkey::_128);
+    key = set_key(AESkey::_256);
+    e = AES(key, AESkey::_128);
 
     e.encryptCBC(input, size, IV);
     std::cout << "\nEncryption with a key of 128 bits:\n" << input << '\n';
@@ -87,3 +94,47 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
+char* set_key(AESkey::Length len) {
+    std::random_device dev; std::mt19937 seed(dev());
+    std::uniform_int_distribution<std::mt19937::result_type> distribution;
+    int i, j;
+    intToChar buff;
+
+    switch(len) {
+        case AESkey::_128:
+            for(i = 0; i < 4; i++) {
+                j = i << 2; // j = i*4
+                buff.integer = distribution(seed);
+                key128[j]   = buff.chars[0];
+                key128[j+1] = buff.chars[1];
+                key128[j+2] = buff.chars[2];
+                key128[j+3] = buff.chars[3];
+            }
+            return key128;
+            break;
+        case AESkey::_192:
+            for(i = 0; i < 6; i++) {
+                j = i << 2; // j = i*4
+                buff.integer = distribution(seed);
+                key192[j]   = buff.chars[0];
+                key192[j+1] = buff.chars[1];
+                key192[j+2] = buff.chars[2];
+                key192[j+3] = buff.chars[3];
+            }
+            return key192;
+            break;
+        case AESkey::_256:
+            for(i = 0; i < 8; i++) {
+                j = i << 2; // j = i*4
+                buff.integer = distribution(seed);
+                key256[j]   = buff.chars[0];
+                key256[j+1] = buff.chars[1];
+                key256[j+2] = buff.chars[2];
+                key256[j+3] = buff.chars[3];
+            }
+            return key256;
+            break;
+        default:
+            throw "Key length not allowed.";
+    }
+}
