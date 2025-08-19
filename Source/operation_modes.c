1e23391329df06446819edd8a94a28f518586218
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-08-18 23:02:05 -0600

    Defines function for encryption and decryption of blocks of bytes pointed by row pointers. Defines functions for encyption and decryption using ECV operation mode.

--- Source/operation_modes.c
+++ Source/operation_modes.c
@@ -1,23 +1,53 @@
-#include"cipher.h"
 #include"operation_modes.h"
 
-void encryptECB(const uint8_t*const input, size_t size, uint8_t*const output){
-  if(size == 0) return;
-  if(input == NULL) return;
-  const uint8_t* currentBlock = input;
-  Block bufferIn, bufferOut;
+void encryptBlockBytes(const uint8_t*const input, const Block *keyExpansion, Nk nk, uint8_t* output){
+  Block buffer;
+  blockFromBytes(input, &buffer);
+  encryptBlock(&buffer, keyExpansion, nk, &buffer, false);
+  bytesFromBlock(&buffer, output);
+}
+
+void decryptBlockBytes(const uint8_t*const input, const Block *keyExpansion, Nk nk, uint8_t* output){
+  Block buffer;
+  blockFromBytes(input, &buffer);
+  decryptBlock(&buffer, keyExpansion, nk, &buffer);
+  bytesFromBlock(&buffer, output);
+}
+
+void encryptECB(const uint8_t*const input, size_t size, const Block *keyExpansion, Nk nk, uint8_t*const output){
+  if(size == 0 || input == NULL) return;
+  const uint8_t* inputCurrentPossition = input;
+  uint8_t* outputCurrentPossition = output;
   size_t numBlocks = size >> 4, numBlocks_1 = numBlocks - 1;                    //  numBlocks = size / 16.
-  size_t rem = size & 15, i;                                                // -Bytes remaining rem = size % 16
+  size_t rem = size & 15, i;                                                    // -Bytes remaining rem = size % 16
 
   for(i = 0; i < numBlocks_1; i++) {
-    blockFromBytes(currentBlock, &bufferIn);
-    encryptBlock(const Block *input, const Block *keyExpansion, Nk nk, Block *output, bool debug);
-    currentBlock += BLOCK_SIZE;
-    }
-  if(rem != 0) {                                                              // -This part of the code is for encrypt input that its size is not multiple of 16.
-      encryptBlock(currentBlock);                                         //  This is not specified in the NIST standard.
-      encryptBlock(currentBlock + rem);                                   // -Not handling the case size < 16
-      return;
+    encryptBlockBytes(inputCurrentPossition, keyExpansion, nk, outputCurrentPossition);
+    inputCurrentPossition += BLOCK_SIZE;
+    outputCurrentPossition += BLOCK_SIZE;
   }
-  encryptBlock(currentBlock);
+  encryptBlockBytes(inputCurrentPossition, keyExpansion, nk, outputCurrentPossition); // -Handling the case where input size is not multiple of 16.
+  if(rem != 0) {                                                                //  This is not specified in the NIST standard.
+    encryptBlockBytes(inputCurrentPossition + rem, keyExpansion, nk, outputCurrentPossition + rem);
+  }
+  // -Not handling the case size < 16
+}
+
+void decryptECB(const uint8_t*const input, size_t size, const Block *keyExpansion, Nk nk, uint8_t*const output){
+  if(size == 0 || input == NULL) return;
+  const uint8_t* inputCurrentPossition = input;
+  uint8_t* outputCurrentPossition = output;
+  size_t numBlocks = size >> 4, numBlocks_1 = numBlocks - 1;                    //  numBlocks = size / 16.
+  size_t rem = size & 15, i;                                                    // -Bytes remaining rem = size % 16
+
+  for(i = 0; i < numBlocks_1; i++) {
+    decryptBlockBytes(inputCurrentPossition, keyExpansion, nk, outputCurrentPossition);
+    inputCurrentPossition += BLOCK_SIZE;
+    outputCurrentPossition += BLOCK_SIZE;
+  }
+  if(rem != 0) {                                                                // -This part of the code is for encrypt input that its size is not multiple of 16.
+    decryptBlockBytes(inputCurrentPossition + rem, keyExpansion, nk, outputCurrentPossition + rem); //  This is not specified in the NIST standard.
+  }
+  decryptBlockBytes(inputCurrentPossition, keyExpansion, nk, outputCurrentPossition);
+  // -Not handling the case size < 16
 }
