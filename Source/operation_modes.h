d0a7ee40adba2a66fe88c07f92ce56fe6db96684
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-08-18 23:01:31 -0600

    Declares function for encryption and decryption of blocks of bytes pointed by row pointers. Declares functions for encyption and decryption using ECV operation mode.

--- Source/operation_modes.h
+++ Source/operation_modes.h
@@ -1,8 +1,27 @@
-#include<stdint.h>
-#include<stddef.h>
+#include"cipher.h"
+
+/*
+ * Takes 16 bytes, converts to block, encrypts and writes on output.
+ * If input == output, original data will be rewritten with encrypted data.
+ * */
+void encryptBlockBytes(const uint8_t*const input, const Block *keyExpansion, Nk nk, uint8_t* output);
+
+/*
+ * Takes 16 bytes, converts to block, decrypts and writes on output.
+ * If input == output, original data will be rewritten with decrypted data.
+ * */
+void decryptBlockBytes(const uint8_t*const input, const Block *keyExpansion, Nk nk, uint8_t* output);
 
 /*
  * Encrypts the data pointed by 'input' using ECB operation mode.
  * The resulting data is written on the location pointed by 'output'.
+ * If input == output (they point to the same location), the input data will be overwritten with the encrypted data.
  * */
-void encryptECB(const uint8_t*const input, size_t size, uint8_t*const output);
+void encryptECB(const uint8_t*const input, size_t size, const Block *keyExpansion, Nk nk, uint8_t*const output);
+
+/*
+ * Decrypts the data pointed by 'input' using ECB operation mode.
+ * The resulting data is written on the location pointed by 'output'.
+ * If input == output (they point to the same location), the input data will be overwritten with the decrypted data.
+ * */
+void decryptECB(const uint8_t*const input, size_t size, const Block *keyExpansion, Nk nk, uint8_t*const output);
