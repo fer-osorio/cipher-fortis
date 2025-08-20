0d054e17875b1577bdb71b536cbc2e649b9801c8
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-08-19 22:11:57 -0600

    Declare CBC encryption and decryption funtions. Add comments

--- Source/operation_modes.h
+++ Source/operation_modes.h
@@ -1,18 +1,6 @@
 #include"cipher.h"
 
 /*
- * Takes 16 bytes, converts to block, encrypts and writes on output.
- * If input == output, original data will be rewritten with encrypted data.
- * */
-void encryptBlockBytes(const uint8_t*const input, const Block *keyExpansion, Nk nk, uint8_t* output);
-
-/*
- * Takes 16 bytes, converts to block, decrypts and writes on output.
- * If input == output, original data will be rewritten with decrypted data.
- * */
-void decryptBlockBytes(const uint8_t*const input, const Block *keyExpansion, Nk nk, uint8_t* output);
-
-/*
  * Encrypts the data pointed by 'input' using ECB operation mode.
  * The resulting data is written on the location pointed by 'output'.
  * If input == output (they point to the same location), the input data will be overwritten with the encrypted data.
@@ -25,3 +13,19 @@ void encryptECB(const uint8_t*const input, size_t size, const Block *keyExpansio
  * If input == output (they point to the same location), the input data will be overwritten with the decrypted data.
  * */
 void decryptECB(const uint8_t*const input, size_t size, const Block *keyExpansion, Nk nk, uint8_t*const output);
+
+/*
+ * Encrypts the data pointed by 'input' using CBC operation mode.
+ * The resulting data is written on the location pointed by 'output'.
+ * If input == output (they point to the same location), the input data will be overwritten with the encrypted data.
+ * IV argument carries a reference to the initial vector intended to be used for the encryption.
+ * */
+void encryptCBC(const uint8_t*const input, size_t size, const Block *keyExpansion, Nk nk, const uint8_t* IV, uint8_t*const output);
+
+/*
+ * Decrypts the data pointed by 'input' using CBC operation mode.
+ * The resulting data is written on the location pointed by 'output'.
+ * If input == output (they point to the same location), the input data will be overwritten with the decrypted data.
+ * IV argument carry a reference to the initial vector used for encryption.
+ * */
+void decryptCBC(const uint8_t*const input, size_t size, const Block *keyExpansion, Nk nk, const Block* IV, uint8_t*const output);
