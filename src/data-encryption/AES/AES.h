f668f768098073d48c15a66185c25d88cd67609e
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-08-21 23:48:22 -0600

    Delete definitions of Word and Block unions (defines in AES.c). Declares KeyExpansionWriteBytes function.

--- src/data-encryption/AES/AES.h
+++ src/data-encryption/AES/AES.h
@@ -12,30 +12,15 @@ extern "C" {
 #define NK128 4
 #define NK192 6
 #define NK256 8
-#define NKMAX 8
 #define Nb 4                                                                    // AES standard constant, length of blocks in words
 enum Nk_{Nk128 = NK128, Nk192 = NK192, Nk256 = NK256};
 
 #define WORD_SIZE 4
-#define WORD_SIZE_SHORTS 2
-#define WORD_LASTIND 3                                                          // -Last index of a word
-#define WORD_LASTIND_SHORT 1                                                    // -Last index of a word using short's
-typedef union Word_ {
-  uint8_t  uint08_[WORD_SIZE];
-  uint16_t uint16_[WORD_SIZE_SHORTS];
-  uint32_t uint32_;
-} Word ;
-
 #define BLOCK_SIZE 16
-#define BLOCK_SIZE_INT64 2
-typedef union Block_{
-    uint8_t  uint08_[BLOCK_SIZE];
-    Word     word_[Nb];
-    uint64_t uint64_[BLOCK_SIZE_INT64];
-} Block ;
 
-struct KeyExpansion;
-typedef struct KeyExpansion* KeyExpansion_ptr;
+typedef union Word_ Word;
+typedef union Block_ Block;
+typedef struct KeyExpansion_ KeyExpansion;
 
 /*
  * Creates a Block instance from the bytes pointed by source. Basically it takes pieces of four bytes and creates the columns with them
@@ -59,24 +44,29 @@ void printBlock(const Block* b, const char* rowHeaders[4]);
  * Builds key expansion object and returns a pointer to it.
  * Consider: Allocates memory using malloc.
  * */
-KeyExpansion_ptr KeyExpansionBuildNew(const Word key[], enum Nk_ Nk, bool debug);
+KeyExpansion* KeyExpansionBuildNew(const uint8_t* key, size_t nk, bool debug);
 
 /*
  * Free the memory allocated for an KeyExpansion object pointed by *ke_pp.
  * */
-void KeyExpansionDelete(KeyExpansion_ptr* ke_pp);
+void KeyExpansionDelete(KeyExpansion** ke_pp);
+
+/*
+ * Write the bytes that forms the key expansion object on the location pointed by dest.
+ * */
+void KeyExpansionWriteBytes(const KeyExpansion* source, uint8_t* dest);
 
 /*
  * Encrypts input block using the key referenced by key_p, the resultant encrypted block is written in output
  * If input == output (they point to the same memory location), the input block is overwritten with the encrypted data
  * */
-void encryptBlock(const Block* input, const KeyExpansion_ptr ke_p, Block* output, bool debug);
+void encryptBlock(const Block* input, const KeyExpansion* ke_p, Block* output, bool debug);
 
 /*
  * Decrypts input block using the key referenced by key_p, the resultant decrypted block is written in output
  * If input == output (they point to the same memory location), the input block is overwritten with the encrypted data
  * */
-void decryptBlock(const Block* input, const KeyExpansion_ptr ke_p, Block* output);
+void decryptBlock(const Block* input, const KeyExpansion* ke_p, Block* output);
 
 #ifdef __cplusplus
 }
