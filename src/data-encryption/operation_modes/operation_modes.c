9d8fd1daad1fa074a68972ff9d5c07740ca9ac47
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-08-23 00:39:36 -0600

    Define decryptCBC__ function.

--- src/data-encryption/operation_modes/operation_modes.c
+++ src/data-encryption/operation_modes/operation_modes.c
@@ -1,3 +1,4 @@
+#include<string.h>
 #include"operation_modes.h"
 #include"../AES/AES.h"
 
@@ -33,10 +34,10 @@ static bool InputOutputHandlerIsSmallerThanBlock(const struct InputOutputHandler
  * If input == output, original data will be rewritten with encrypted data.
  * */
 static void encryptBlockBytes(const uint8_t*const input, const KeyExpansion* ke_p, uint8_t* output){
-  Block* buffer;
-  blockFromBytes(input, buffer);
+  Block* buffer = BlockFromBytes(input);
   encryptBlock(buffer, ke_p, buffer, false);
   bytesFromBlock(buffer, output);
+  free(buffer);
 }
 
 /*
@@ -51,10 +52,10 @@ static void InputOutputHandlerEncryptBlockBytes(const struct InputOutputHandler*
  * If input == output, original data will be rewritten with decrypted data.
  * */
 static void decryptBlockBytes(const uint8_t*const input, const KeyExpansion* ke_p, uint8_t* output){
-  Block* buffer;
-  blockFromBytes(input, buffer);
-  decryptBlock(buffer, ke_p, buffer);
+  Block* buffer = BlockFromBytes(input);
+  decryptBlock(buffer, ke_p, buffer, false);
   bytesFromBlock(buffer, output);
+  free(buffer);
 }
 
 /*
@@ -93,34 +94,72 @@ static void decryptECB__(const KeyExpansion* ke_p, struct InputOutputHandler* io
 /*
  * Build Block with input, build Block with XORsource, xors both blocks and encrypt the result.
  * */
-static void encryptXorBlockBytes(const uint8_t*const input, const KeyExpansion* ke_p, const uint8_t* XORsource, uint8_t* output){
-  Block* buffer;
-  blockFromBytes(input, buffer);
-  XORequalBlockWithBytes(buffer, XORsource);
+static void xorEncryptBlockBytes(const uint8_t*const input, const KeyExpansion* ke_p, const uint8_t* XORsource, uint8_t* output){
+  Block* buffer = BlockFromBytes(input);
+  BlockXORequalBytes(buffer, XORsource);
   encryptBlock(buffer, ke_p, buffer, false);
   bytesFromBlock(buffer, output);
+  free(buffer);
 }
 
 /*
- * Implements encryptXorBlockBytes on ioh object.
+ * Implements xorEncryptBlockBytes on ioh object.
  * */
-static void InputOutputHandlerEncryptXorBlockBytes(const struct InputOutputHandler* ioh, const uint8_t *XORsource, const KeyExpansion* ke_p){
-  encryptXorBlockBytes(ioh->inputCurrentPossition, ke_p, XORsource, ioh->outputCurrentPossition);
+static void InputOutputHandlerXorEncryptBlockBytes(const struct InputOutputHandler* ioh, const uint8_t *XORsource, const KeyExpansion* ke_p){
+  xorEncryptBlockBytes(ioh->inputCurrentPossition, ke_p, XORsource, ioh->outputCurrentPossition);
 }
 
-static void encryptCBC__(const KeyExpansion* ke_p, const uint8_t* IV,struct InputOutputHandler* ioh){
+static void encryptCBC__(const KeyExpansion* ke_p, const uint8_t* IV, struct InputOutputHandler* ioh){
   if(InputOutputHandlerIsSmallerThanBlock(ioh)) return;                         // -Not handling the case size < 16
   const uint8_t* inputPreviousBlock;
-  InputOutputHandlerEncryptXorBlockBytes(ioh, IV, ke_p);                        // -Encryption of the first block.
+  InputOutputHandlerXorEncryptBlockBytes(ioh, IV, ke_p);                        // -Encryption of the first block.
   for(size_t i = 1; i < ioh->sizeInBlocks; i++) {                               // -Encryption of the rest of the blocks.
     inputPreviousBlock = ioh->inputCurrentPossition;
     InputOutputHandlerMoveForwardOneBlock(ioh);
-    InputOutputHandlerEncryptXorBlockBytes(ioh, inputPreviousBlock, ke_p);
+    InputOutputHandlerXorEncryptBlockBytes(ioh, inputPreviousBlock, ke_p);
   }
   // -Handling the case where input size is not multiple of 16. This is not specified in the NIST standard. Not specified in the NIST standard.
   if(ioh->tailSize != 0) {
       size_t k = ioh->sizeInBlocks*BLOCK_SIZE, i;
       for(i = 0; i < ioh->tailSize; i++,k++) ioh->output[k] ^= inputPreviousBlock[i];
       encryptBlockBytes(ioh->inputCurrentPossition + ioh->tailSize, ke_p, ioh->outputCurrentPossition + ioh->tailSize);
+  }
+}
+
+/*
+ * Build Block with input, build Block with XORsource, xors both blocks and decrypt the result.
+ * */
+static void decryptXorBlockBytes(const uint8_t*const input, const KeyExpansion* ke_p, const uint8_t* XORsource, uint8_t* output){
+  Block* buffer = BlockFromBytes(input);
+  decryptBlock(buffer, ke_p, buffer, false);
+  BlockXORequalBytes(buffer, XORsource);
+  bytesFromBlock(buffer, output);
+  free(buffer);
+}
+
+/*
+ * Implements xorEncryptBlockBytes on ioh object.
+ * */
+static void InputOutputHandlerDecryptXorBlockBytes(const struct InputOutputHandler* ioh, const uint8_t *XORsource, const KeyExpansion* ke_p){
+  decryptXorBlockBytes(ioh->inputCurrentPossition, ke_p, XORsource, ioh->outputCurrentPossition);
+}
+
+static void decryptCBC__(const KeyExpansion* ke_p, const uint8_t* IV, struct InputOutputHandler* ioh) {
+  if(InputOutputHandlerIsSmallerThanBlock(ioh)) return;                         // -Not handling the case size < 16
+  uint8_t prevBlockCopy[BLOCK_SIZE];
+  uint8_t currBlockCopy[BLOCK_SIZE];
+  memcpy(prevBlockCopy, ioh->inputCurrentPossition, BLOCK_SIZE);
+  InputOutputHandlerDecryptXorBlockBytes(ioh, IV, ke_p);                        // -Decryption of the first block.
+  for(size_t i = 1; i < ioh->sizeInBlocks; i++) {                               // -Encryption of the rest of the blocks.
+    InputOutputHandlerMoveForwardOneBlock(ioh);
+    memcpy(currBlockCopy, ioh->inputCurrentPossition, BLOCK_SIZE);
+    InputOutputHandlerDecryptXorBlockBytes(ioh, prevBlockCopy, ke_p);
+    memcpy(prevBlockCopy, currBlockCopy, BLOCK_SIZE);
+  }
+  // -Handling the case where input size is not multiple of 16. This is not specified in the NIST standard. Not specified in the NIST standard.
+  if(ioh->tailSize != 0) {
+    decryptBlockBytes(ioh->inputCurrentPossition + ioh->tailSize, ke_p, ioh->outputCurrentPossition + ioh->tailSize);
+    size_t k = ioh->sizeInBlocks*BLOCK_SIZE, i;
+    for(i = 0; i < ioh->tailSize; i++,k++) ioh->output[k] ^= prevBlockCopy[i];
   }
 }
