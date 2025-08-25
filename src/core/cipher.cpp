1ea945323be53e1f8df27c6bb8706ce014e2a0b2
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-08-25 15:46:25 -0600

    Update constants

--- src/core/cipher.cpp
+++ src/core/cipher.cpp
@@ -1,4 +1,4 @@
-#include"../../include/AESencryption.hpp"
+#include"../../include/cipher.hpp"
 #include"../data-encryption/AES/AES.h"
 #include"../data-encryption/operation_modes/operation_modes.h"
 #include"../utils/print_bytes/print_bytes.hpp"
@@ -6,17 +6,17 @@
 using namespace AESencryption;
 
 Cipher::Cipher() {
-    this->keyExpansion = new uint8_t[this->keyExpLen];
-    for(int i = 0; i < this->keyExpLen; i++) this->keyExpansion[i] = 0;         // -Since the key constitutes of just zeros, key expansion is also just zeros
+    this->keyExpansion = new uint8_t[this->keyExpansionLength];
+    for(int i = 0; i < this->keyExpansionLength; i++) this->keyExpansion[i] = 0;         // -Since the key constitutes of just zeros, key expansion is also just zeros
 }
 
-Cipher::Cipher(const Key& k) :key(k), Nk((int)k.getLenBytes() >> 2), Nr(Nk+6), keyExpLen((Nr+1)<<4) {
+Cipher::Cipher(const Key& k) :key(k), Nk(k.getLenBytes() / 4), Nr(Nk+6), keyExpansionLength((Nr+1)<<4) {
     this->buildKeyExpansion();
 }
 
-Cipher::Cipher(const Cipher& a) : key(a.key), Nk(a.Nk), Nr(a.Nr), keyExpLen(a.keyExpLen) {
-    this->keyExpansion = new uint8_t[(unsigned)a.keyExpLen];
-    for(int i = 0; i < a.keyExpLen; i++) this->keyExpansion[i] = a.keyExpansion[i];
+Cipher::Cipher(const Cipher& a) : key(a.key), Nk(a.Nk), Nr(a.Nr), keyExpansionLength(a.keyExpansionLength) {
+    this->keyExpansion = new uint8_t[(unsigned)a.keyExpansionLength];
+    for(int i = 0; i < a.keyExpansionLength; i++) this->keyExpansion[i] = a.keyExpansion[i];
 }
 
 Cipher::~Cipher() {
@@ -27,14 +27,14 @@ Cipher::~Cipher() {
 Cipher& Cipher::operator = (const Cipher& a) {
     if(this != &a) {
         this->key = a.key;
-        if(this->keyExpLen != a.keyExpLen) {
+        if(this->keyExpansionLength != a.keyExpansionLength) {
             this->Nk = a.Nk;
             this->Nr = a.Nr;
-            this->keyExpLen = a.keyExpLen;
+            this->keyExpansionLength = a.keyExpansionLength;
             if(this->keyExpansion != NULL) delete[] keyExpansion;
-            this->keyExpansion = new uint8_t[(unsigned)a.keyExpLen];
+            this->keyExpansion = new uint8_t[(unsigned)a.keyExpansionLength];
         }
-        for(int i = 0; i < a.keyExpLen; i++) this->keyExpansion[i] = a.keyExpansion[i];
+        for(int i = 0; i < a.keyExpansionLength; i++) this->keyExpansion[i] = a.keyExpansion[i];
     }
     return *this;
 }
@@ -43,17 +43,17 @@ std::ostream& AESencryption::operator<<(std::ostream& ost, const Cipher& c) {
     ost << "AES::Cipher object information:\n";
     ost << c.key;
     ost << "\tNr: " << c.Nr << " rounds\n";
-    ost << "\tKey Expansion size: " << c.keyExpLen << " bytes\n";
+    ost << "\tKey Expansion size: " << c.keyExpansionLength << " bytes\n";
     ost << "\tKey Expansion:";
 
     const size_t bytes_per_row = 32;
     size_t bytes_to_print;
     if (c.keyExpansion) {
-        for(size_t i = 0; i < c.keyExpLen; i += bytes_per_row) {
+        for(size_t i = 0; i < c.keyExpansionLength; i += bytes_per_row) {
             ost << "\n\t\t"; // Start each new line of the expansion
             // Determine how many bytes to print in this row (handles the last partial row). Here, we are supposing KeyLenExp is a multiple of 32,
             // which is true for AES standard.
-            bytes_to_print = (i + bytes_per_row > c.keyExpLen) ? (c.keyExpLen - i) : bytes_per_row;
+            bytes_to_print = (i + bytes_per_row > c.keyExpansionLength) ? (c.keyExpansionLength - i) : bytes_per_row;
             print_bytes_as_hex(ost, &c.keyExpansion[i], bytes_to_print);
         }
     } else {
@@ -65,7 +65,7 @@ std::ostream& AESencryption::operator<<(std::ostream& ost, const Cipher& c) {
 
 void Cipher::buildKeyExpansion() {
     KeyExpansion* ke_p = KeyExpansionBuildNew(this->key.data, this->Nk, false);
-    if(this->keyExpansion == NULL) this->keyExpansion = new uint8_t[this->keyExpLen];
+    if(this->keyExpansion == NULL) this->keyExpansion = new uint8_t[this->keyExpansionLength];
     KeyExpansionWriteBytes(ke_p, this->keyExpansion);
     KeyExpansionDelete(&ke_p);
 }
