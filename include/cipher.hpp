83f1721d9860149aec5d71c5fe8156e2da17cf73
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-08-25 14:59:12 -0600

    New file. Handle cipher objects.

---
+++ include/cipher.hpp
@@ -0,0 +1,36 @@
+#ifndef CIPHER_HPP
+#define CIPHER_HPP
+
+#include"key.hpp"
+
+namespace AESencryption {
+
+std::ostream& operator << (std::ostream& st, const Cipher& c);			// -Declaration here so this function is inside the name space function.
+
+class Cipher {
+private:
+	// -The default values for a cipher object are the values for a key of 256 bits
+	Key key = Key();
+	int Nk = 8, Nr = 14, keyExpLen = 240;
+	uint8_t* keyExpansion = NULL;
+public:
+	Cipher();								// -The default constructor will set the key expansion as zero in every element.
+	Cipher(const Key&);
+	Cipher(const Cipher& a);
+	~Cipher();
+
+	Cipher& operator = (const Cipher& a);
+	friend std::ostream& operator << (std::ostream& st, const Cipher& c);
+
+	void encrypt(const uint8_t*const data, size_t size, uint8_t*const output)const;	// -Encrypts using operation mode stored in Key object
+	void decrypt(const uint8_t*const data, size_t size, uint8_t*const output)const;	// -Decrypts using operation mode stored in Key object
+
+	void saveKey(const char*const fname) const{ this->key.save(fname); }
+	Key::OpMode getOpMode() const{ return this->key.getOpMode(); }
+
+	private:
+	void buildKeyExpansion();						// -Creates key expansion
+	void formInitialVector();						// -Creates initial vector and writes it on destination array
+};
+};
+#endif
