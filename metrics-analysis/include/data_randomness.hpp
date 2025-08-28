fcadf78c73981a4723e82e5072f2fca33df31eaf
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-08-27 18:31:58 -0600

    Declare class to measure the randomens of a data set.

---
+++ metrics-analysis/include/data_randomness.hpp
@@ -0,0 +1,30 @@
+#include<cstddef>
+#include<stdint.h>
+#include<vector>
+
+class DataRandomness{								// -Specialized to handle data from raw bytes
+private:
+	struct RandMetrics{
+		double Entropy;
+		double ChiSquare;
+		double CorrelationAdjacentByte;
+	};
+	struct RandMetrics* rmetrics = NULL;
+	size_t data_size = 0;
+	size_t byteValueFrequence[256];
+
+	void calculate_entropy();
+	void calculate_ChiSquare();
+
+public:
+	DataRandomness() = default;						// -Rule of Zero: No need for manual copy/assignment/destructor.
+										//  The compiler-generated ones are correct.
+	~DataRandomness();
+	explicit DataRandomness(const std::vector<std::byte>& data);
+
+	double calculateCorrelation(const std::vector<std::byte>& data, size_t offset) const;
+
+	double getEntropy() const noexcept;
+	double getChiSquare() const noexcept;
+	double getCorrelationAdjacentByte() const noexcept;
+};
