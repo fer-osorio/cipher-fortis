f2a039bf908cbeab29e155318cbe3f485d59449d
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-08-28 19:18:22 -0600

    Update header files. Delete unnecesry code.

--- file-handlers/include/textf.hpp
+++ file-handlers/include/textf.hpp
@@ -1,34 +1,10 @@
-#include"../../include/cipher.hpp"
+#include"../include/file_base.hpp"
 #include<iostream>
 
-class TXT {									// -Handling .txt files
-	char* name 	= NULL;
-	char* content	= NULL;							// -Text file content.
-	unsigned size	= 0;
-
+class TXT : private FileBase {
 	public:
-	TXT() : name() {}							// -Just for type declaration.
-	TXT(const char* fname);							// -Building from file.
-	TXT(const TXT&);
-	~TXT() {
-		if(this->name != NULL) delete [] this->name;
-		this->name = NULL;
-		if(this->content != NULL) delete[] this->content;
-		this->content = NULL;
-		this->size = 0;
-	}
+	TXT(const std::filesystem::path& path);					// -Building from file.
 
-	TXT& operator = (const TXT&);
-	void save(const char* fname = NULL) const;
-	void printName() const{ if(this->name != NULL) std::cout << this->name; }
-
-	friend void encrypt(TXT& txt, AESencryption::Cipher& e) {				// -Encrypts using the operation mode defined in Key object
-		e.encrypt(txt.content, txt.size);
-		txt.save();							// -The reason of the existence of these friend functions is to be capable of
-	}									//  encrypt and decrypt many files with the same Cipher object while maintaining
-										//  attributes of txt object private
-	friend void decrypt(TXT& txt, AESencryption::Cipher& e) {				// -Decrypts using the operation mode defined in Key object
-		e.decrypt(txt.content, txt.size);
-		txt.save();
-	}
+	bool load() override;							// Override, loading as text
+	bool save(const std::filesystem::path& output_path) const override;	// Override, saving as text
 };
