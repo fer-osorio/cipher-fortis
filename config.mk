0b8911cccdf7bf5e56b597f7ea3c1c9e00bedfd2
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-08-31 12:17:49 -0600

    Create file to handle project configurations

---
+++ config.mk
@@ -0,0 +1,4 @@
+# Include paths
+COMMON_INCLUDES = -I$(PROJECT_ROOT)/include
+C_INCLUDES = $(COMMON_INCLUDES) -I$(PROJECT_ROOT)/data-encryption/include
+CXX_INCLUDES = $(COMMON_INCLUDES) -I$(PROJECT_ROOT)/file-handlers/include
