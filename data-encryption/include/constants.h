e28a2ba7e28b3c3fd33f5d1c50a41de2213b47a2
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-09-08 10:59:38 -0600

    Change lower case to upper case. Erase equa symbol. Define enumeration.

--- data-encryption/include/constants.h
+++ data-encryption/include/constants.h
@@ -1,20 +1,23 @@
+// Constants defined using NIST standard or derived from it.
 #ifndef _CONSTANTS_
 #define _CONSTANTS_
 
-#define BLOCK_SIZE = 16;
+#define WORD_SIZE 4
+#define BLOCK_SIZE 16
 
-#define Nb = 4;
+#define NB 4;
 
-#define Nk128 = 4;
-#define Nk192 = 6;
-#define Nk256 = 8;
+#define NK128 4
+#define NK192 6
+#define NK256 8
+enum Nk_{Unknown, Nk128 = NK128, Nk192 = NK192, Nk256 = NK256};
 
-#define Nr128 = 10;
-#define Nr192 = 12;
-#define Nr256 = 14;
+#define NR128 10;
+#define NR192 12;
+#define NR256 14;
 
-#define keyExpansionLength128 = 44;
-#define keyExpansionLength192 = 52;
-#define keyExpansionLength256 = 60;
+#define KEY_EXPANSION_LENGTH_128 44;
+#define KEY_EXPANSION_LENGTH_192 52;
+#define KEY_EXPANSION_LENGTH_256 60;
 
 #endif
