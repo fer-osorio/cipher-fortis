74bab38693cc0c70ff9dd5aae3a3c17bfc9add7a
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-09-08 14:34:40 -0600

    Add attributes. Cipher object takes the responsability of encryption configuration.

--- include/cipher.hpp
+++ include/cipher.hpp
@@ -6,19 +6,54 @@
 
 namespace AESencryption {
 
+struct InitVector;
+
 std::ostream& operator << (std::ostream& st, const Cipher& c);			// -Declaration here so this function is inside the name space function.
 
 class Cipher : public Encryptor {
-private:
-	// -The default values for a cipher object are the values for a key of 128 bits
-	Key key = Key();
-	size_t Nk = AESconstants::Nk128;
-	size_t Nr = AESconstants::Nr128;
-	size_t keyExpansionLength = AESconstants::keyExpansionLength128;
-	uint8_t* keyExpansion = NULL;
 public:
+	struct OperationMode{
+	public:
+		enum struct Identifier {
+			ECB,							// -Electronic Code Book (not recommended).
+			CBC,							// -Cipher Block Chaining.
+		};
+	private:
+		Identifier ID_;
+		InitVector* IV_ = NULL;						// -Initial vector in case of CBC operation mode
+
+	public:
+		OperationMode(Identifier);
+		OperationMode(const OperationMode&);
+		OperationMode& operator=(const OperationMode&);
+		~OperationMode();
+		Identifier getOperationModeID() const;
+		const uint8_t* getIVpointerData() const;
+
+	};
+	struct Config{
+	private:
+		OperationMode operationMode;
+		size_t _Nk;
+		size_t Nr;
+		size_t keyExpansionLengthBytes;
+	public:
+		Config(OperationMode::Identifier opModeID, size_t Nk);
+		OperationMode::Identifier getOperationModeID() const;
+		size_t getNk() const;
+		size_t getNr() const;
+		size_t getKeyExpansionLengthBytes() const;
+		const uint8_t* getIVpointerData() const;
+	};
+private:
+	Key key = Key();
+	uint8_t* keyExpansion = NULL;
+	struct Config config;
+
 	Cipher();								// -The default constructor will set the key expansion as zero in every element.
-	Cipher(const Key&);
+
+public:
+	Cipher(const Key&, const OperationMode::Identifier);
 	Cipher(const Cipher& a);
 	~Cipher();
 
@@ -28,10 +63,14 @@ public:
 	void encryption(std::vector<uint8_t>& data) const override;
 	void decryption(std::vector<uint8_t>& data) const override;
 
-	void saveKey(const char*const fname) const{ this->key.save(fname); }
-	Key::OpMode getOpMode() const{ return this->key.getOpMode(); }
+	void saveKey(const char*const fname) const;
+	OperationMode getOptModeID() const;
 
 	private:
+	/*
+	void set_IV(const InitVector source);					// -Sets initial vector by copying the array passed as argument
+	bool IVisInitialized() const { return this->initializedIV; }
+	*/
 	void buildKeyExpansion();						// -Creates key expansion
 	void formInitialVector();						// -Creates initial vector and writes it on destination array
 	void encrypt(const uint8_t*const data, size_t size, uint8_t*const output)const;	// -Encrypts using operation mode stored in Key object
