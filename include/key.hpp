0b89687f3cf5a8c79614a059c9f6af515b51b066
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-09-08 14:33:43 -0600

    Remove atributtes. Leaving the responsability of encryption configuration to cipher object.

--- include/key.hpp
+++ include/key.hpp
@@ -1,16 +1,11 @@
 #ifndef KEY_HPP
 #define KEY_HPP
 
-#include"constants.hpp"
 #include<iostream>
 #include<stddef.h>
 #include<stdint.h>
 
 namespace AESencryption {
-
-struct InitVector{
-	uint8_t data[AESconstants::BLOCK_SIZE];
-};
 
 struct Key;									// -Declaring struct Key and class Cipher to use them as arguments in functions
 std::ostream& operator << (std::ostream& ost, const Key& k);
@@ -20,28 +15,16 @@ class Cipher;									// Declare cipher class, so we can make it a friend of Key
 struct Key {
 public:
 	enum struct LenBits {_128 = 128,_192 = 192,_256 = 256};			// -Allowed AES key lengths
-	enum struct OpMode {
-		ECB,								// -Electronic Code Book (not recommended).
-		CBC,								// -Cipher Block Chaining.
-	};
 private:
 	uint8_t*data = NULL;
 	LenBits	lenBits;							// -Length in bits.
 	size_t	lenBytes;							// -Length in bytes.
-	OpMode	opMode_;
-	bool initializedIV  = false;						// -Tells if the initial vector is already initialized or not
-	InitVector IV = {							// -Initial vector for the CBC operation mode
-		0, 0, 0, 0,
-		0, 0, 0, 0,
-		0, 0, 0, 0,
-		0, 0, 0, 0
-	};
 
 	friend Cipher;
 	// The following two private constructors can only be acceced by Cipher class, the intention is to have well-constructed keys for the user.
 	Key();
-	Key(LenBits, OpMode);
-	Key(const uint8_t* const _key, LenBits, OpMode);
+	Key(LenBits);
+	Key(const uint8_t* const _key, LenBits);
 public:
 	Key(const char*const fname);						// -Building from binary file.
 	Key(const Key&);
@@ -51,13 +34,10 @@ public:
 	bool operator == (const Key&) const;
 	friend std::ostream& operator << (std::ostream& ost, const Key& k);
 
-	OpMode getOpMode() const{ return this->opMode_; }
 	size_t getLenBytes() const {return this->lenBytes;}
 
 	void save(const char*const fname) const;				// -Saving information in a binary file.
 private:
-	void set_IV(const InitVector source);					// -Sets initial vector by copying the array passed as argument
-	bool IVisInitialized() const { return this->initializedIV; }
 	// -Writes IV in destination
 	// -Warning: We are supposing we have at least 16 bytes of space in destination
 	void write_IV(uint8_t*const destination) const;
