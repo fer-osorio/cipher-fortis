e24db302228fa08855ea53531b573856ecc5fd02
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-09-08 10:52:25 -0600

    Create file. Unify constant definitions

---
+++ data-encryption/include/constants.h
@@ -0,0 +1,20 @@
+#ifndef _CONSTANTS_
+#define _CONSTANTS_
+
+#define BLOCK_SIZE = 16;
+
+#define Nb = 4;
+
+#define Nk128 = 4;
+#define Nk192 = 6;
+#define Nk256 = 8;
+
+#define Nr128 = 10;
+#define Nr192 = 12;
+#define Nr256 = 14;
+
+#define keyExpansionLength128 = 44;
+#define keyExpansionLength192 = 52;
+#define keyExpansionLength256 = 60;
+
+#endif
