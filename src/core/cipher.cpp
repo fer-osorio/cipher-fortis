3ba25b07b05e3f2ec19a0d2f6fd65b39ed2482a0
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-09-08 16:51:12 -0600

    Define default constructors. Define buildOperationMode function. Update encryption functions.

--- src/core/cipher.cpp
+++ src/core/cipher.cpp
@@ -1,7 +1,14 @@
-#include"../../include/cipher.hpp"
-#include"../../data-encryption/include//AES.h"
+#include"../../data-encryption/include/constants.h"
+#include"../../data-encryption/include/AES.h"
 #include"../../data-encryption/include/operation_modes.h"
+#include"../../include/cipher.hpp"
 #include"../utils/include/print_bytes.hpp"
+#include<cstring>
+#include<chrono>
+
+struct AESencryption::InitVector{
+    uint8_t data[BLOCK_SIZE];
+};
 
 using namespace AESencryption;
 
@@ -12,23 +19,100 @@ static size_t getNrFromNk(size_t Nk){
     return Nk+6;
 }
 static size_t getKeyExpansionByteLenFromNr(size_t Nr){
-    return (Nr+1)*Nb*4;
+    return (Nr+1)*NB*4;
 }
 
-Cipher::Cipher() {
-    this->keyExpansion = new uint8_t[this->keyExpansionLength];
-    for(size_t i = 0; i < this->keyExpansionLength; i++) this->keyExpansion[i] = 0;         // -Since the key constitutes of just zeros, key expansion is also just zeros
+Cipher::OperationMode::OperationMode(){}
+
+Cipher::OperationMode::OperationMode(Identifier ID) : ID_(ID){
+    switch(ID){
+        case Identifier::ECB:
+            break;
+        case Identifier::CBC:
+            this->IV_ = new InitVector;
+            break;
+    }
 }
 
-Cipher::Cipher(const Key& k)
-    :key(k), Nk(getNkfromLenbit(k.lenBits)), Nr(getNrFromNk(Nk)), keyExpansionLength(getKeyExpansionByteLenFromNr(Nr)) {
+Cipher::OperationMode::OperationMode(const OperationMode& optMode): ID_(optMode.ID_){
+    if(optMode.IV_ != NULL){
+        this->IV_ = new InitVector;
+        std::memcpy(this->IV_->data, optMode.IV_, BLOCK_SIZE);
+    }
+}
+
+Cipher::OperationMode& Cipher::OperationMode::operator=(const OperationMode& optMode){
+    if(this != &optMode){
+        this->ID_ = optMode.ID_;
+        if(optMode.IV_!=NULL){
+            if(this->IV_ == NULL) this->IV_ = new InitVector;
+            std::memcpy(this->IV_->data, optMode.IV_, BLOCK_SIZE);
+        } else {
+            delete this->IV_; this->IV_ = NULL;
+        }
+    }
+    return *this;
+}
+
+Cipher::OperationMode::Identifier Cipher::OperationMode::getOperationModeID() const{
+    return this->ID_;
+}
+
+const uint8_t* Cipher::OperationMode::getIVpointerData() const{
+    return this->IV_->data;
+}
+
+Cipher::OperationMode::~OperationMode(){
+    if(this->IV_ != NULL) delete this->IV_;
+}
+
+Cipher::OperationMode Cipher::OperationMode::buildInCBCmode(const InitVector& IVsource){
+    OperationMode optMode(OperationMode::Identifier::CBC);
+    std::memcpy(optMode.IV_->data, IVsource.data, BLOCK_SIZE);
+    return optMode;
+}
+
+Cipher::Config::Config(): _Nk(NK128), Nr(NR128), keyExpansionLengthBytes(KEY_EXPANSION_LENGTH_128) {}
+
+Cipher::Config::Config(OperationMode optMode, size_t Nk)
+    :operationMode(optMode), _Nk(Nk), Nr(getNrFromNk(Nk)), keyExpansionLengthBytes(getKeyExpansionByteLenFromNr(this->Nr)){
+}
+
+Cipher::OperationMode::Identifier Cipher::Config::getOperationModeID() const{
+    return this->operationMode.getOperationModeID();
+}
+
+size_t Cipher::Config::getNk() const{
+    return this->_Nk;
+}
+
+size_t Cipher::Config::getNr() const{
+    return this->Nr;
+}
+
+size_t Cipher::Config::getKeyExpansionLengthBytes() const{
+    return this->keyExpansionLengthBytes;
+}
+
+const uint8_t* Cipher::Config::getIVpointerData() const{
+    return this->operationMode.getIVpointerData();
+}
+
+Cipher::Cipher(): config(OperationMode::Identifier::ECB, Nk128) {
+    size_t keyExpLen = this->config.getKeyExpansionLengthBytes();
+    this->keyExpansion = new uint8_t[keyExpLen];
+    for(size_t i = 0; i < keyExpLen; i++) this->keyExpansion[i] = 0;            // -Since the key constitutes of just zeros, key expansion is also just zeros
+}
+
+Cipher::Cipher(const Key& k, const OperationMode::Identifier optModeID): key(k) {
     this->buildKeyExpansion();
+    this->config = Config(this->buildOperationMode(optModeID), getNkfromLenbit(k.lenBits));
 }
 
-Cipher::Cipher(const Cipher& a)
-    : key(a.key), Nk(a.Nk), Nr(a.Nr), keyExpansionLength(a.keyExpansionLength) {
-    this->keyExpansion = new uint8_t[static_cast<size_t>(a.keyExpansionLength)];
-    for(size_t i = 0; i < a.keyExpansionLength; i++) this->keyExpansion[i] = a.keyExpansion[i];
+Cipher::Cipher(const Cipher& c): key(c.key), config(c.config) {
+    size_t keyExpLen = c.config.getKeyExpansionLengthBytes();
+    this->keyExpansion = new uint8_t[keyExpLen];
+    for(size_t i = 0; i < keyExpLen; i++) this->keyExpansion[i] = c.keyExpansion[i];
 }
 
 Cipher::~Cipher() {
@@ -36,17 +120,16 @@ Cipher::~Cipher() {
     keyExpansion = NULL;
 }
 
-Cipher& Cipher::operator = (const Cipher& a) {
-    if(this != &a) {
-        this->key = a.key;
-        if(this->keyExpansionLength != a.keyExpansionLength) {
-            this->Nk = a.Nk;
-            this->Nr = a.Nr;
-            this->keyExpansionLength = a.keyExpansionLength;
+Cipher& Cipher::operator = (const Cipher& c) {
+    if(this != &c) {
+        size_t ckeyExpLen = config.getKeyExpansionLengthBytes();
+        this->key = c.key;
+        if(this->config.getKeyExpansionLengthBytes() != ckeyExpLen) {
             if(this->keyExpansion != NULL) delete[] keyExpansion;
-            this->keyExpansion = new uint8_t[static_cast<size_t>(a.keyExpansionLength)];
+            this->keyExpansion = new uint8_t[ckeyExpLen];
         }
-        for(size_t i = 0; i < a.keyExpansionLength; i++) this->keyExpansion[i] = a.keyExpansion[i];
+        std::memcpy(this->keyExpansion, c.keyExpansion, ckeyExpLen);
+        this->config = c.config;
     }
     return *this;
 }
@@ -54,18 +137,19 @@ Cipher& Cipher::operator = (const Cipher& a) {
 std::ostream& AESencryption::operator<<(std::ostream& ost, const Cipher& c) {
     ost << "AES::Cipher object information:\n";
     ost << c.key;
-    ost << "\tNr: " << c.Nr << " rounds\n";
-    ost << "\tKey Expansion size: " << c.keyExpansionLength << " bytes\n";
+    ost << "\tNr: " << c.config.getNr() << " rounds\n";
+    ost << "\tKey Expansion size: " << c.config.getKeyExpansionLengthBytes() << " bytes\n";
     ost << "\tKey Expansion:";
 
     const size_t bytes_per_row = 32;
     size_t bytes_to_print;
+    size_t cKeyExpLen = c.config.getKeyExpansionLengthBytes();
     if (c.keyExpansion) {
-        for(size_t i = 0; i < c.keyExpansionLength; i += bytes_per_row) {
+        for(size_t i = 0; i < cKeyExpLen; i += bytes_per_row) {
             ost << "\n\t\t"; // Start each new line of the expansion
             // Determine how many bytes to print in this row (handles the last partial row). Here, we are supposing KeyLenExp is a multiple of 32,
             // which is true for AES standard.
-            bytes_to_print = (i + bytes_per_row > c.keyExpansionLength) ? (c.keyExpansionLength - i) : bytes_per_row;
+            bytes_to_print = (i + bytes_per_row > cKeyExpLen) ? (cKeyExpLen - i) : bytes_per_row;
             print_bytes_as_hex(ost, &c.keyExpansion[i], bytes_to_print);
         }
     } else {
@@ -75,40 +159,57 @@ std::ostream& AESencryption::operator<<(std::ostream& ost, const Cipher& c) {
     return ost;
 }
 
+Cipher::OperationMode Cipher::buildOperationMode(const OperationMode::Identifier optModeID){
+    switch(optModeID){
+        case OperationMode::Identifier::ECB:
+            return OperationMode(optModeID);
+            break;
+        case OperationMode::Identifier::CBC:
+            InitVector IVbuff;
+            union {
+                uint8_t  data08[BLOCK_SIZE];
+                uint64_t data64[2];
+            } tt;
+            tt.data64[0] = std::chrono::high_resolution_clock::now().time_since_epoch().count();
+            tt.data64[1] = tt.data64[0]++;
+            if(this->keyExpansion != NULL)
+                encryptECB(tt.data08, BLOCK_SIZE, this->keyExpansion, this->config.getNk(), IVbuff.data);
+            return OperationMode::buildInCBCmode(IVbuff);
+            break;
+    }
+}
+
 void Cipher::buildKeyExpansion() {
-    KeyExpansion* ke_p = KeyExpansionMemoryAllocationBuild(this->key.data, this->Nk, false);
-    if(this->keyExpansion == NULL) this->keyExpansion = new uint8_t[this->keyExpansionLength];
+    KeyExpansion* ke_p = KeyExpansionMemoryAllocationBuild(this->key.data, this->config.getNk(), false);
+    if(this->keyExpansion == NULL) this->keyExpansion = new uint8_t[this->config.getKeyExpansionLengthBytes()];
     KeyExpansionWriteBytes(ke_p, this->keyExpansion);
     KeyExpansionDelete(&ke_p);
 }
 
-void Cipher::formInitialVector(){                                               // -Simple method for setting the initial vector.
-    setInitialVector(this->key.IV.data);
-    this->key.initializedIV = true;
-}
-
 void Cipher::encrypt(const uint8_t*const data, size_t size, uint8_t*const output) const{
     if(size == 0 || data == NULL) return;
-    Key::OpMode opMode = this->key.getOpMode();
-    switch(opMode) {
-        case Key::OpMode::ECB:
-            encryptECB(data, size, this->keyExpansion, this->Nk, output);
+    size_t thisNk = this->config.getNk();
+    OperationMode::Identifier optMode = this->config.getOperationModeID();
+    switch(optMode) {
+        case OperationMode::Identifier::ECB:
+            encryptECB(data, size, this->keyExpansion, thisNk, output);
             break;
-        case Key::OpMode::CBC:
-            encryptCBC(data, size, this->keyExpansion, this->Nk, this->key.IV.data, output);
+        case OperationMode::Identifier::CBC:
+            encryptCBC(data, size, this->keyExpansion, thisNk, this->config.getIVpointerData(), output);
             break;
     }
 }
 
 void Cipher::decrypt(const uint8_t*const data, size_t size, uint8_t*const output) const{
     if(size == 0 || data == NULL) return;
-    Key::OpMode opMode = this->key.getOpMode();
-    switch(opMode) {
-        case Key::OpMode::ECB:
-            decryptECB(data, size, this->keyExpansion, this->Nk, output);
+    size_t thisNk = this->config.getNk();
+    OperationMode::Identifier optMode = this->config.getOperationModeID();
+    switch(optMode) {
+        case OperationMode::Identifier::ECB:
+            decryptECB(data, size, this->keyExpansion, thisNk, output);
             break;
-        case Key::OpMode::CBC:
-            decryptCBC(data, size, this->keyExpansion, this->Nk, this->key.IV.data, output);
+        case OperationMode::Identifier::CBC:
+            decryptCBC(data, size, this->keyExpansion, thisNk, this->config.getIVpointerData(), output);
             break;
     }
 }
@@ -120,3 +221,6 @@ void Cipher::encryption(std::vector<uint8_t>& data) const{
 void Cipher::decryption(std::vector<uint8_t>& data) const{
     decrypt(data.data(), data.size(), data.data());
 }
+
+void Cipher::saveKey(const char*const fname) const{ this->key.save(fname); }
+Cipher::OperationMode Cipher::getOptModeID() const{ return this->config.getOperationModeID(); }
