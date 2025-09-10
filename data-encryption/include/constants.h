651cff41eab3153da740d38717a4fdb8cb2b0283
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-09-10 13:48:29 -0600

    Add enumeration for key length in bits

--- data-encryption/include/constants.h
+++ data-encryption/include/constants.h
@@ -10,15 +10,21 @@
 #define NK128 4
 #define NK192 6
 #define NK256 8
-enum Nk_{Unknown, Nk128 = NK128, Nk192 = NK192, Nk256 = NK256};
+enum Nk_t{
+  UnknownNk, Nk128 = NK128, Nk192 = NK192, Nk256 = NK256
+};
 
 #define NR128 10
 #define NR192 12
 #define NR256 14
 
-#define KEY_LENBITS_128 128
-#define KEY_LENBITS_192 192
-#define KEY_LENBITS_256 256
+#define KEY_LENGTH_BITS_128 128
+#define KEY_LENGTH_BITS_192 192
+#define KEY_LENGTH_BITS_256 256
+enum KeylenBits_t{
+  UnknownKeylenBits, Keylenbits128 = KEY_LENGTH_BITS_128,
+  Keylenbits192 = KEY_LENGTH_BITS_192, Keylenbits256 = KEY_LENGTH_BITS_256
+};
 
 #define KEY_EXPANSION_LENGTH_128_WORDS 44
 #define KEY_EXPANSION_LENGTH_192_WORDS 52
