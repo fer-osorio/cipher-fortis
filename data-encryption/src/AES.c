7d7d32811bab8ace47b181e6d6bc1b4818592ca0
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-09-12 13:20:20 -0600

    Define function KeyExpansionBuildWrite

--- data-encryption/src/AES.c
+++ data-encryption/src/AES.c
@@ -481,6 +481,15 @@ const uint8_t* KeyExpansionReturnBytePointerToData(const KeyExpansion_t*const ke
   return (uint8_t*)ke_p->dataBlocks;
 }
 
+enum ExceptionCode KeyExpansionBuildWrite(const uint8_t* key, size_t keylenbits, uint8_t* dest, bool debug){
+  KeyExpansion_t* ke_p = KeyExpansionMemoryAllocationBuild(key, keylenbits, false);
+  if(ke_p == NULL) return NullKeyExpansion;
+  if(dest == NULL) return NullDestination;
+  KeyExpansionWriteBytes(ke_p, dest);
+  KeyExpansionDelete(&ke_p);
+  return NoException;
+}
+
 enum ExceptionCode encryptBlock(const Block_t* input, const KeyExpansion_t* ke_p, Block_t* output, bool debug) {
   size_t i, j;
   // -Debugging purposes. Columns of the debugging table.
