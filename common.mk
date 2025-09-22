425baf9bd3e026e4ab50c79bdba7419e4aec3d3e
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-09-22 15:56:33 -0600

    Define functions. Define find_local_sources and sources to objects functions.

--- common.mk
+++ common.mk
@@ -104,6 +104,19 @@ define create_executable
 	$(call print_success,"Created executable:\\n\\t\\t$(1)")
 endef
 
+# Function to find all source files in local directory
+# Usage: $(call find_local_sources,extension)
+define find_local_sources
+$(or $(patsubst ./%,%,$(shell find . -name "*.$(1)" 2>/dev/null)),$(warning No $(1) files found))
+endef
+
+# Function to convert source files to object files
+# Usage: OBJECTS = $(call sources_to_objects,sources,build_prefix)
+# If $(1) has .cpp as suffix, the suffix gets "cutted" and the $(2)/obj/>>the rest of $(1)<<.o is the output
+define sources_to_objects
+$(patsubst %.cpp,$(2)/obj/%.o,$(1))
+endef
+
 # Clean function. Standard setting
 define clean_standard
 	$(call print_info,"Cleaning $(OBJDIR)")
