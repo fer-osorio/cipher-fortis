bab73d5229cda588e8884013ba4c301d07f593e7
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-10-04 12:55:06 -0600

    File moved

--- tests/system/command-line-tools/image-encryption/test_workflows_bmp_encryptor.cpp
+++ tests/system/command-line-tools/image-encryption/test_workflows_bmp_encryptor.cpp
@@ -1,11 +1,13 @@
 // System/E2E Testing Examples for AES File Encryption Tool
 // File: system/test_file_encryption_workflows.cpp
 
-#include "../include/test_framework.hpp"
+#include "../../include/test_framework.hpp"
 #include <filesystem>
 #include <fstream>
 #include <cstdlib>
 #include <string>
+
+#define BMP_ENCRYPTOR ../bin/command-line-tools/image-encryption/bmp_encryptor
 
 namespace SystemTestUtils {
     // Helper function to execute command line tool
