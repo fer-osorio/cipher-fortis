0e9509f8637e29e5fe649d4ef92dd59af4cdefa7
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-10-05 12:32:15 -0600

    Modify load and save functions. Return void and throw runtime_error exception at failure

--- file-handlers/src/textf.cpp
+++ file-handlers/src/textf.cpp
@@ -6,31 +6,36 @@ using namespace File;
 
 TXT::TXT(const std::filesystem::path& path) : FileBase(path) {}
 
-bool TXT::load(){
+void TXT::load(){
     std::ifstream file;
     file.open(this->file_path, std::ios::ate);
     if (!file.is_open()) {
-        return false;
+        throw std::runtime_error(
+            "In member function void TXT::load(): Could not open file."
+        );
     }
     std::streamsize size = file.tellg();
     file.seekg(0, std::ios::beg);
     this->data.resize(size);
-    if (file.read(reinterpret_cast<char*>(this->data.data()), size)) {
-        return true;
+    if (!file.read(reinterpret_cast<char*>(this->data.data()), size)) {
+        this->data.clear(); // Clear data on failure
+        throw std::runtime_error(
+            "In member function void TXT::load(): Could not read file."
+        );
     }
-    this->data.clear(); // Clear data on failure
-    return false;
 }
 
-bool TXT::save(const std::filesystem::path& output_path) const{
+void TXT::save(const std::filesystem::path& output_path) const{
     std::ofstream file;
     file.open(this->file_path);
     if(!file.is_open()) {
-        return false;
+        throw std::runtime_error(
+            "In member function void TXT::save(const std::filesystem::path& output_path) const: Could not open file."
+        );
     }
-    if(file.write(reinterpret_cast<const char*>(this->data.data()), this->data.size())){
-        file.close();
-        return true;
+    if(!file.write(reinterpret_cast<const char*>(this->data.data()), this->data.size())){
+        throw std::runtime_error(
+            "In member function void TXT::save(const std::filesystem::path& output_path) const: Could not write file."
+        );
     }
-    return false;
 }
