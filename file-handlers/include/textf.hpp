7b54d22161b0fa8be6962586ccdd6dc3e8783dcf
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-10-05 12:30:53 -0600

    Modify return value

--- file-handlers/include/textf.hpp
+++ file-handlers/include/textf.hpp
@@ -7,8 +7,8 @@ class TXT : private FileBase {
 	public:
 	TXT(const std::filesystem::path& path);					// -Building from file.
 
-	bool load() override;							// Override, loading as text
-	bool save(const std::filesystem::path& output_path) const override;	// Override, saving as text
+	void load() override;							// Override, loading as text
+	void save(const std::filesystem::path& output_path) const override;	// Override, saving as text
 };
 
 } // namespace File
