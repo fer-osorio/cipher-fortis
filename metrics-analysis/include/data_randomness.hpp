090dfff1ff1600f3c51c3ed7ee360227e050eea6
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-10-31 13:38:44 -0600

    Add header file. Make DataRandomness a derived class of Statistical Metrics. Make compute-random-metrics functions virtual. Comment default constructor and makes destructor virtual

--- metrics-analysis/include/data_randomness.hpp
+++ metrics-analysis/include/data_randomness.hpp
@@ -1,8 +1,9 @@
 #include<cstddef>
 #include<stdint.h>
 #include<vector>
+#include"statistical_metrics.hpp"
 
-class DataRandomness{								// -Specialized to handle data from raw bytes
+class DataRandomness: StatisticalMetrics {								// -Specialized to handle data from raw bytes
 private:
 	struct RandomnessMetrics{
 		double Entropy;
@@ -13,16 +14,17 @@ private:
 	size_t data_size = 0;
 	size_t byteValueFrequence[256];
 
-	void calculate_entropy();
-	void calculate_ChiSquare();
+	virtual void get_byteValueFrequence(const std::vector<std::byte>& data);
+	virtual void calculate_entropy();
+	virtual void calculate_ChiSquare();
 
 public:
-	DataRandomness() = default;						// -Rule of Zero: No need for manual copy/assignment/destructor.
+	//DataRandomness() = default;						// -Rule of Zero: No need for manual copy/assignment/destructor.
 										//  The compiler-generated ones are correct.
-	~DataRandomness();
 	explicit DataRandomness(const std::vector<std::byte>& data);
+	virtual ~DataRandomness();
 
-	double calculateCorrelation(const std::vector<std::byte>& data, size_t offset) const;
+	static double calculateCorrelation(const std::vector<std::byte>& data, size_t offset);
 
 	double getEntropy() const noexcept;
 	double getChiSquare() const noexcept;
