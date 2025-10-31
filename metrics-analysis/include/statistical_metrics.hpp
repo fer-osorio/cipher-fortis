2e38b789cedab9762c41c2719ca20de63a0e1e24
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-10-30 22:51:10 -0600

    Create file

---
+++ metrics-analysis/include/statistical_metrics.hpp
@@ -0,0 +1,11 @@
+#ifndef STATISTICAL_METRICS
+#define STATISTICAL_METRICS
+
+#include <vector>
+#include <cstdint>
+
+class StatisticalMetrics {
+	virtual ~StatisticalMetrics() = default;
+};
+
+#endif
