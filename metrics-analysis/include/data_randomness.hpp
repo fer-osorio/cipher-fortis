1bba0f698aa6bb05544026bdea3db1ed15a026c8
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-10-31 17:45:22 -0600

    Add function arguments jump_size and start for sub-array analysis. Create calculateDataRandomnessSubArray and calculateCorrelationSubArray to obtain measurements from sub-arrays

--- metrics-analysis/include/data_randomness.hpp
+++ metrics-analysis/include/data_randomness.hpp
@@ -9,21 +9,34 @@ private:
 		double ChiSquare;
 		double CorrelationAdjacentByte;
 	};
-	struct RandomnessMetrics* rmetrics = NULL;
+	struct RandomnessMetrics* rmetrics = nullptr;
 	size_t data_size = 0;
-	size_t byteValueFrequence[256];
+	size_t byteValueFrequence[256] = {0};
 
-	virtual void get_byteValueFrequence(const std::vector<std::byte>& data);
-	virtual void calculate_entropy();
-	virtual void calculate_ChiSquare();
+	/**
+	 * Functions include jump_size to be capable of analysing structured data sets. For example, analyse the randomness metrics for the green component of the
+	 * pixels of an image.
+	 */
+	void get_byteValueFrequence(const std::vector<std::byte>& data, size_t start, size_t jump_size);
+	void calculate_entropy();
+	void calculate_ChiSquare();
+	static double calculateCorrelationSubArray(const std::vector<std::byte>& data, size_t start, size_t jump_size, size_t offset);
+
+	DataRandomness() = default;						// -Private constructor, only accessible inside the class
+	DataRandomness(const DataRandomness&);					// -Not allowing copy contructor or
+	DataRandomness& operator = (const DataRandomness&);			//  copy assigment
 
 public:
-	//DataRandomness() = default;						// -Rule of Zero: No need for manual copy/assignment/destructor.
-										//  The compiler-generated ones are correct.
 	explicit DataRandomness(const std::vector<std::byte>& data);
-	virtual ~DataRandomness();
+	~DataRandomness();
 
 	static double calculateCorrelation(const std::vector<std::byte>& data, size_t offset);
+	/**
+	 * @brief Computes the randomness metrics on the bytes holded by the 'data' vector and separated by an specyfied amount.
+	 * @param jump_size The separation of the bytes being analyzed.
+	 * @return DataRandomness class instance containing the result.
+	*/
+	static DataRandomness calculateDataRandomnessSubArray(const std::vector<std::byte>& data, size_t start, size_t jump_size);
 
 	double getEntropy() const noexcept;
 	double getChiSquare() const noexcept;
