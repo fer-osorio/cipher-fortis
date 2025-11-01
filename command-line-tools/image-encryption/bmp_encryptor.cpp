a8e49936119d7c9307c253fa7eb1a37f88d63be8
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-11-01 13:26:23 -0600

    Update namespace

--- command-line-tools/image-encryption/bmp_encryptor.cpp
+++ command-line-tools/image-encryption/bmp_encryptor.cpp
@@ -1,30 +1,30 @@
 #include "../../include/cipher.hpp"
 #include "../../file-handlers/include/bitmap.hpp"
-#include "../../CLI/include/cli_config.hpp"
+#include "../../crypto-cli/include/cli_config.hpp"
 
 int main(int argc, const char* argv[]){
     // Parsing arguments. Converting user intput to valid arguments for this program.
-    const CLI::CryptoConfig cryp_conf= CLI::ArgumentParser(argc,argv).parse();
+    const CryptoCLI::CryptoConfig cryp_conf= CryptoCLI::ArgumentParser(argc,argv).parse();
     if(!cryp_conf.is_valid){
         std::cerr << cryp_conf.error_message;
         return 1;
     }
 
     try{
-        if(cryp_conf.operation == CLI::CryptoConfig::Operation::ENCRYPT
-            || cryp_conf.operation == CLI::CryptoConfig::Operation::DECRYPT){
+        if(cryp_conf.operation == CryptoCLI::CryptoConfig::Operation::ENCRYPT
+            || cryp_conf.operation == CryptoCLI::CryptoConfig::Operation::DECRYPT){
             // Resource acquisition
             File::Bitmap bmp(cryp_conf.input_file);
             AESencryption::Cipher ciph(cryp_conf.create_key(), cryp_conf.operation_mode);
             bmp.load();
             // Data processing: Encryption
-            if(cryp_conf.operation == CLI::CryptoConfig::Operation::ENCRYPT) bmp.apply_encryption(ciph);
-            if(cryp_conf.operation == CLI::CryptoConfig::Operation::DECRYPT) bmp.apply_decryption(ciph);
+            if(cryp_conf.operation == CryptoCLI::CryptoConfig::Operation::ENCRYPT) bmp.apply_encryption(ciph);
+            if(cryp_conf.operation == CryptoCLI::CryptoConfig::Operation::DECRYPT) bmp.apply_decryption(ciph);
             bmp.save(cryp_conf.output_file);
             // Return with success status
             return 0;
         }
-        if(cryp_conf.operation == CLI::CryptoConfig::Operation::GENERATE_KEY){
+        if(cryp_conf.operation == CryptoCLI::CryptoConfig::Operation::GENERATE_KEY){
             AESencryption::Key key(cryp_conf.key_length);
             key.save(cryp_conf.output_file.c_str());
             return 0;
