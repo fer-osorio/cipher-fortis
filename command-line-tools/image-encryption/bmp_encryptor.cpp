10f20c23a0888b82ca07dd5cafb1e6a918e4748c
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-11-02 13:20:45 -0600

    Add metrics analysis feature

--- command-line-tools/image-encryption/bmp_encryptor.cpp
+++ command-line-tools/image-encryption/bmp_encryptor.cpp
@@ -1,6 +1,10 @@
 #include "../../include/cipher.hpp"
 #include "../../file-handlers/include/bitmap.hpp"
+#include "../../metrics-analysis/include/data_randomness.hpp"
 #include "../../crypto-cli/include/cli_config.hpp"
+#include <memory>
+
+void output_metrics_results(const DataRandomness& input_metrics, const DataRandomness& output_metrics);
 
 int main(int argc, const char* argv[]){
     // Parsing arguments. Converting user intput to valid arguments for this program.
@@ -13,13 +17,21 @@ int main(int argc, const char* argv[]){
     try{
         if(cryp_conf.operation == CLIConfig::CryptoConfig::Operation::ENCRYPT
             || cryp_conf.operation == CLIConfig::CryptoConfig::Operation::DECRYPT){
-            // Resource acquisition
             File::Bitmap bmp(cryp_conf.input_file);
             AESencryption::Cipher ciph(cryp_conf.create_key(), cryp_conf.operation_mode);
+            std::unique_ptr<DataRandomness> input_rand = nullptr;
+            std::unique_ptr<DataRandomness> output_rand = nullptr;
             bmp.load();
-            // Data processing: Encryption
+            // Data processing
+            if(cryp_conf.show_metrics){
+                input_rand = std::make_unique<DataRandomness>(bmp.calculate_randomness());
+            }
             if(cryp_conf.operation == CLIConfig::CryptoConfig::Operation::ENCRYPT) bmp.apply_encryption(ciph);
             if(cryp_conf.operation == CLIConfig::CryptoConfig::Operation::DECRYPT) bmp.apply_decryption(ciph);
+            if(cryp_conf.show_metrics){
+                output_rand = std::make_unique<DataRandomness>(bmp.calculate_randomness());
+                output_metrics_results(*input_rand, *output_rand);
+            }
             bmp.save(cryp_conf.output_file);
             // Return with success status
             return 0;
@@ -35,4 +47,22 @@ int main(int argc, const char* argv[]){
     }
 
     return 0;
+}
+
+void output_metrics_results(const DataRandomness& input_metrics, const DataRandomness& output_metrics){
+    std::cout << "{\n";
+    std::cout << "  \"data_analysis\": [\n";
+    std::cout << "      \"input_metrics\": {\n";
+    std::cout << "          \"entropy\": " << input_metrics.getEntropy() << ",\n";
+    std::cout << "          \"correlation\": " << input_metrics.getCorrelationAdjacentByte() << ",\n";
+    std::cout << "          \"xi_square\": " << input_metrics.getChiSquare() << "\n";
+    std::cout << "      },\n";
+    std::cout << "      \"output_metrics\": {\n";
+    std::cout << "          \"entropy\": " << output_metrics.getEntropy() << ",\n";
+    std::cout << "          \"correlation\": " << output_metrics.getCorrelationAdjacentByte() << ",\n";
+    std::cout << "          \"xi_square\": " << output_metrics.getChiSquare() << "\n";
+    std::cout << "      }\n";
+    std::cout << "  ]";
+    std::cout << "\n";
+    std::cout << "}\n";
 }
