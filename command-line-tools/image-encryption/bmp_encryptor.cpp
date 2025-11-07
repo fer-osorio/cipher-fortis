c7719326a53e862e92c3eb32c41c106cae64a59b
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-11-07 00:57:41 -0600

    Implement namespace. Creating operation mode object from cryp_conf object

--- command-line-tools/image-encryption/bmp_encryptor.cpp
+++ command-line-tools/image-encryption/bmp_encryptor.cpp
@@ -6,19 +6,24 @@
 
 void output_metrics_results(const DataRandomness& input_metrics, const DataRandomness& output_metrics);
 
+using namespace AESencryption;
+using namespace CLIConfig;
+using namespace File;
+
 int main(int argc, const char* argv[]){
     // Parsing arguments. Converting user intput to valid arguments for this program.
-    const CLIConfig::CryptoConfig cryp_conf= CLIConfig::ArgumentParser(argc,argv).parse();
+    const CryptoConfig cryp_conf= ArgumentParser(argc,argv).parse();
     if(!cryp_conf.is_valid){
         std::cerr << cryp_conf.error_message;
         return 1;
     }
 
     try{
-        if(cryp_conf.operation == CLIConfig::CryptoConfig::Operation::ENCRYPT
-            || cryp_conf.operation == CLIConfig::CryptoConfig::Operation::DECRYPT){
-            File::Bitmap bmp(cryp_conf.input_file);
-            AESencryption::Cipher ciph(cryp_conf.create_key(), cryp_conf.operation_mode);
+        if(cryp_conf.operation == CryptoConfig::Operation::ENCRYPT
+            || cryp_conf.operation == CryptoConfig::Operation::DECRYPT){
+            Bitmap bmp(cryp_conf.input_file);
+            Cipher::OperationMode optmode = cryp_conf.create_optmode();
+            Cipher cipher(cryp_conf.create_key(), optmode);
             std::unique_ptr<DataRandomness> input_rand = nullptr;
             std::unique_ptr<DataRandomness> output_rand = nullptr;
             bmp.load();
@@ -26,18 +31,22 @@ int main(int argc, const char* argv[]){
             if(cryp_conf.show_metrics){
                 input_rand = std::make_unique<DataRandomness>(bmp.calculate_randomness());
             }
-            if(cryp_conf.operation == CLIConfig::CryptoConfig::Operation::ENCRYPT) bmp.apply_encryption(ciph);
-            if(cryp_conf.operation == CLIConfig::CryptoConfig::Operation::DECRYPT) bmp.apply_decryption(ciph);
+            if(cryp_conf.operation == CryptoConfig::Operation::ENCRYPT) bmp.apply_encryption(cipher);
+            if(cryp_conf.operation == CryptoConfig::Operation::DECRYPT) bmp.apply_decryption(cipher);
             if(cryp_conf.show_metrics){
                 output_rand = std::make_unique<DataRandomness>(bmp.calculate_randomness());
                 output_metrics_results(*input_rand, *output_rand);
             }
             bmp.save(cryp_conf.output_file);
+            // In the following case, operation mode object was not loaded from file, so we will save it.
+            if(cryp_conf.mode_file.empty()){
+                optmode.save(cryp_conf.output_file.string() + ".optmode");
+            }
             // Return with success status
             return 0;
         }
-        if(cryp_conf.operation == CLIConfig::CryptoConfig::Operation::GENERATE_KEY){
-            AESencryption::Key key(cryp_conf.key_length);
+        if(cryp_conf.operation == CryptoConfig::Operation::GENERATE_KEY){
+            Key key(cryp_conf.key_length);
             key.save(cryp_conf.output_file.c_str());
             return 0;
         }
