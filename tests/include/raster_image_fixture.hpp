8935d6ca8b00750685b2a22f688e85c6b8a52d95
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2026-04-19 21:35:54 +0000

    refactor(tests): migrate RasterImageFixture to TestEnvironment RAII
    Replaces the four private env-management methods with a TestEnvironment
    member. The three public static createValid* methods become thin wrappers
    into TestUtils::Raster, removing the last stbi_write calls from this file.

--- tests/include/raster_image_fixture.hpp
+++ tests/include/raster_image_fixture.hpp
@@ -3,33 +3,30 @@
 
 #include <gtest/gtest.h>
 #include <filesystem>
+#include "test_environment.hpp"
+#include "raster_asset_utils.hpp"
 
 namespace fs = std::filesystem;
 
 class RasterImageFixture : public ::testing::Test {
 public:
-    static void createValidPng(const fs::path& path, int width, int height);
-    static void createValidBmp(const fs::path& path, int width, int height);
+    static void createValidPng (const fs::path& path, int width, int height);
+    static void createValidBmp (const fs::path& path, int width, int height);
     static void createValidJpeg(const fs::path& path, int width, int height, int quality = 90);
 
 protected:
-    fs::path testDataDir     = fs::path("tests/data/raster_image");
-    fs::path validPngPath    = testDataDir / "valid_10x10.png";
-    fs::path smallPngPath    = testDataDir / "small_2x2.png";
-    fs::path largePngPath    = testDataDir / "large_100x100.png";
-    fs::path corruptPath     = testDataDir / "corrupt.png";
-    fs::path emptyPath       = testDataDir / "empty.png";
-    fs::path nonexistentPath = testDataDir / "does_not_exist.png";
-    fs::path validJpegPath   = testDataDir / "valid_10x10.jpg";
+    TestEnvironment  env_{"tests/data/raster_image"};
+    const fs::path&  testDataDir   = env_.path();
+    fs::path         validPngPath;
+    fs::path         smallPngPath;
+    fs::path         largePngPath;
+    fs::path         corruptPath;
+    fs::path         emptyPath;
+    fs::path         nonexistentPath;
+    fs::path         validJpegPath;
 
     void SetUp() override;
     void TearDown() override;
-
-private:
-    void setupTestEnvironment();
-    void cleanupTestEnvironment();
-    void createCorruptFile(const fs::path& path);
-    void createEmptyFile(const fs::path& path);
 };
 
 #endif // RASTER_IMAGE_FIXTURE_HPP
