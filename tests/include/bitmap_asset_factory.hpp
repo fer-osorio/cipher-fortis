8299fe4e69902aba89bfeb2528f2b20efe9c7de5
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2026-04-22 20:17:53 +0000

    refactor(tests): override verify_roundtrip in BitmapAssetFactory

--- tests/include/bitmap_asset_factory.hpp
+++ tests/include/bitmap_asset_factory.hpp
@@ -6,4 +6,9 @@ public:
     fs::path    make_valid(const fs::path& dir) const override; // 32x32 BMP
     fs::path    make_large(const fs::path& dir) const override; // 4096x4096 BMP
     std::string extension()                     const override; // "bmp"
+
+    bool verify_roundtrip(
+        const fs::path& original,
+        const fs::path& roundtripped
+    ) const override;
 };
