a5a2215b82ebecc5692cf0b3cadba0ce25c076b0
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2026-04-25 12:30:38 -0600

    test(asset_factory): create asset factory for png images

---
+++ tests/src/png_asset_factory.cpp
@@ -0,0 +1,26 @@
+#include "../include/png_asset_factory.hpp"
+#include "../include/raster_asset_utils.hpp"
+#include "../../file-handlers/include/png_image.hpp"
+
+fs::path PngAssetFactory::make_valid(const fs::path& dir) const {
+    fs::path p = dir / "valid.png";
+    TestUtils::Raster::make_png(p, 32, 32);
+    return p;
+}
+
+fs::path PngAssetFactory::make_large(const fs::path& dir) const {
+    fs::path p = dir / "large.png";
+    TestUtils::Raster::make_png(p, 4096, 4096);
+    return p;
+}
+
+std::string PngAssetFactory::extension() const { return "png"; }
+
+bool PngAssetFactory::verify_roundtrip(
+    const fs::path& original,
+    const fs::path& roundtripped
+) const {
+    File::PNG ref(original);
+    ref.load();
+    return ref.verify_saved_file(roundtripped);
+}
